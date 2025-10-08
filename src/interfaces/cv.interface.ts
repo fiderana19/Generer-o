cfc13df233f133997ada1fd7e864de5677d84b72
ANDRIAHERILANTO Antsa Fiderana <antsafider@gmail.com>
2025-10-08 19:04:23 +0300

    feat: Fixed all interfaces for cv data

---
+++ src/interfaces/cv.interface.ts
@@ -0,0 +1,81 @@
+export interface General {
+  firstName: string;
+  lastName: string;
+  title: string; 
+  profileSummary: string;
+}
+
+export interface ContactData {
+  address: string; 
+  email: string;
+  phone: string;
+  website: string;
+  facebook: string;
+  github: string;
+  linkedin: string;
+}
+
+export interface EducationEntry {
+  period: string;
+  title: string;
+  institution: string;
+}
+
+export interface ExperienceEntry {
+  period: string;
+  title: string;
+  company: string;
+  description: string;
+}
+
+export interface SkillsItem {
+  skill: string,
+  level: string
+}
+
+export interface SoftSkillsItem {
+  skill: string,
+}
+
+export interface HobbiesItem {
+  hobby: string,
+}
+
+export interface LanguagesItem {
+  language: string, 
+  level: string
+}
+
+export interface CVData {
+  general: General;
+
+  contact: ContactData;
+  
+  education: EducationEntry[];
+  
+  experiences: ExperienceEntry[]; 
+  
+  skills: SkillsItem[];
+  
+  soft_skills: SoftSkillsItem[];
+
+  hobbies: HobbiesItem[];
+
+  languages: LanguagesItem[],
+}
+
+export const initialCVData: CVData = {
+  general: {   
+    firstName: "VOTRE NOM",
+    lastName: "VOTRE PRÉNOM",
+    title: "Titre du Poste",
+    profileSummary: "Votre profil professionnel ici...",
+  },
+  contact: { email: "", phone: "", facebook: "", linkedin: "",   address: "", github: "", website: "" },
+  education: [],
+  skills: [],
+  soft_skills: [],
+  experiences: [],
+  languages: [],
+  hobbies: []
+};
